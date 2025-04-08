94aeeae832dc54325d2ee9b2c9c431fd20dbfe62
carolina-silisque <carolina.silisque@ucb.edu.bo>
2025-04-08 12:09:26 -0400

    ejercicio 3 terminado

---
+++ TAREAS/tareaIF/ej3.c
@@ -0,0 +1,17 @@
+//3. Evaluar si un año es bisiesto
+
+#include <stdio.h>
+
+int main(void) {
+    int anno = 0;
+    printf("Ingrese un año para determinar si es o no bisiesto:\n");
+    scanf("%d", &anno);
+
+    if ((anno % 4 == 0) && ((anno % 100 != 0) || (anno % 400 == 0))) {
+        printf("El año %d es bisiesto.\n", anno);
+    } else {
+        printf("El año %d no es bisiesto.\n", anno);
+    }
+
+    return 0;
+}
