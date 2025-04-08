b8de6283d81e79319d3bbbd379f3bfbf35346559
carolina-silisque <carolina.silisque@ucb.edu.bo>
2025-04-08 12:05:39 -0400

    ejercicio 1 comparador terminado (if)

---
+++ TAREAS/tareaIF/ej1.c
@@ -0,0 +1,29 @@
+
+// 1. Comparar dos números
+// Consigna: Escribe un programa que solicite al usuario dos números enteros y determine cuál es mayor
+// o si ambos son iguales.
+
+
+#include <stdio.h>
+
+int main(void) {
+    int numberA = 0;
+    int numberB = 0;
+
+    printf("Ingrese el primer numero: \n");
+    scanf("%d", &numberA);
+
+    printf("Ingrese el segundo numero: \n");
+    scanf("%d", &numberB);
+    
+    if (numberA == numberB) {
+        printf("Los numeros son iguales, %d \n", numberA);
+    } else if (numberA > numberB) {
+        printf("El numero mayor es %d \n", numberA);
+    } else {
+        printf("El numero mayor es %d \n", numberB);
+        
+    }
+
+    return 0;
+}
