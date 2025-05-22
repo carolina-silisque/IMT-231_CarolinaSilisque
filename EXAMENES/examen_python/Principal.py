6e63fa334a42b69d5cb03eabdf56cc56365ad453
carolina-silisque <carolina.silisque@ucb.edu.bo>
2025-05-22 11:08:05 -0400

    menu funcionando

---
+++ EXAMENES/examen_python/Principal.py
@@ -0,0 +1,39 @@
+#  from Funciones import .. 
+
+while True:
+    print("--------   MENU DE OPCIONES   -------")
+    print("1. Calcular la suma de todos los divisores de un número N (excluyendo el propio número).")
+    print("2. Generar un triángulo de caracteres con letras del alfabeto hasta una altura N.")
+    print("3. Mostrar los primeros N números primos.")
+    print("4. Generar la secuencia de los primeros N términos de la serie de Fibonacc inversa.")
+    print("5. Salir del programa.")
+    print("Seleccione una opcion: \n")
+
+    opcion = int(input("Ingrese la opción de Menu "))
+    
+
+    if opcion == 1:
+       num = int(input("Ingrese un número: "))
+    #    num_entre_3(num)
+       
+
+    elif opcion == 2:
+        filas= int(input('ingrese el numero de filas(altura) que tendra la piramide\n'))
+        # piramide_asteriscos(filas)
+
+    elif opcion == 3:
+        num = int (input('ingrese un numero '))
+        # serie_alternada(num)
+
+    elif opcion == 4:
+       num = int(input("Ingrese un número: "))
+    #    contar_primos(num)
+
+                                
+    elif opcion == 5 :
+        print("Gracias por usar el programa, Hasta luego!")
+        break
+    else:
+        print("Opcion no valida. Intente nuevamente.\n")
+
+
